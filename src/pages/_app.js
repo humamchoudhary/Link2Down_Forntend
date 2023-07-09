1bdcb19339ceb89c2ef2b84b590496a859d05b1d
humamchoudhary <70004653+humamchoudhary@users.noreply.github.com>
2023-07-09 22:44:54 +0500

    Update _app.js

--- src/pages/_app.js
+++ src/pages/_app.js
@@ -1,6 +1,7 @@
 import "@/styles/globals.css";
 
 export const URL = "https://backend.link2down.com";
+
 export default function App({ Component, pageProps }) {
   return <Component {...pageProps} />;
 }
