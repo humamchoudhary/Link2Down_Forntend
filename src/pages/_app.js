e36e6f093b3ce847dc144749f32b0ecb4e58d114
humamchoudhary <70004653+humamchoudhary@users.noreply.github.com>
2023-07-23 17:33:19 +0500

    ft

--- src/pages/_app.js
+++ src/pages/_app.js
@@ -1,5 +1,11 @@
 import "@/styles/globals.css";
 export const URL = "http://backend.link2down.com";
+import { Analytics } from "@vercel/analytics/react";
 export default function App({ Component, pageProps }) {
-  return <Component {...pageProps} />;
+  return (
+    <>
+      <Component {...pageProps} />;
+      <Analytics />
+    </>
+  );
 }
